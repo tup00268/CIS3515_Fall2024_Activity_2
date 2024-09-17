595e85ab6f70337841c7cbd0440e376f29793ceb
Roy Shimon <tup00268@temple.edu>
2024-09-17 15:23:19 -0400

    Steps 2-4

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import com.google.android.material.datepicker.OnSelectionChangedListener
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +18,26 @@ class MainActivity : AppCompatActivity() {
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
         //val numberArray = Array Of Numbers
 
+        val numberArray = Array(20){i -> (i+1) * 5}
+
         /* TODO Step 3: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
 
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray)
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
 
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long){
+                val selectedValue = parent.getItemAtPosition(position).toString().toFloat()
+
+                displayTextView.textSize = selectedValue
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>){
+
+            }
+        }
     }
 }
